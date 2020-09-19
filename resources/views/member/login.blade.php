dcb3a2d5bd47c6620a2dd5e96290517dc9aaba60
leetaehee <lastride25@naver.com>
2020-09-19 16:42:46 +0900

    이거는 반영하기

--- resources/views/member/login.blade.php
+++ resources/views/member/login.blade.php
@@ -9,7 +9,7 @@
     <div class="form-login">
         <form method="post">
             <div class="text-center mt-3 mb-3">
-                <h3>로그인(확인차원-테스트2)</h3>
+                <h3>로그인(확인차원-테스트3)</h3>
             </div>
             <div class="form-group">
                 <label for="user-id">아이디</label>
