28c75b53c088776dd33dd1a926c17a57b74a5ef8
leetaehee <lastride25@naver.com>
2020-09-19 15:32:56 +0900

    응응

--- resources/views/member/login.blade.php
+++ resources/views/member/login.blade.php
@@ -9,10 +9,10 @@
     <div class="form-login">
         <form method="post">
             <div class="text-center mt-3 mb-3">
-                <h3>로그인(수미)</h3>
+                <h3>로그인</h3>
             </div>
             <div class="form-group">
-                <label for="user-id">아이디222</label>
+                <label for="user-id">아이디</label>
                 <input type="text"
                        id="user-id"
                        name="user_id"
