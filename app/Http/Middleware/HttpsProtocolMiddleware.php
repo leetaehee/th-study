9d5c4b97306a068d11b425a19403da8764e04749
leetaehee <lastride25@naver.com>
2020-11-02 22:24:35 +0900

    복구2

--- app/Http/Middleware/HttpsProtocolMiddleware.php
+++ app/Http/Middleware/HttpsProtocolMiddleware.php
@@ -16,7 +16,7 @@ class HttpsProtocolMiddleware
     public function handle($request, Closure $next)
     {
         if (!$request->secure() && app()->environment('production')) {
-            return redirect()->secure($request->getRequestUri());
+            //return redirect()->secure($request->getRequestUri());
         }
 
         return $next($request);
